172b24717b020c9ac5ef2fcfcdd5db52baa56fd6
Ion Efrem <efremion5@gmail.com>
2018-05-20 12:17:43 +0200

    Buttons, Div und Canvas Style.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-alert("Extern");
+init();
+
+function init()
+{
+    background_canvas=document.getElementById('background_canvas');
+    background_ctx=background_canvas.getContext('2d');
+    main_canvas=document.getElementById('main_canvas');
+    main_ctx=main_canvas.getContext('2d');
+
+}
+
