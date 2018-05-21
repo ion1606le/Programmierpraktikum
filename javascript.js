98029b1621a24370fb91c7dd9ccd3d4acf4218fd
Ion Efrem <efremion5@gmail.com>
2018-05-21 23:55:42 +0200

    Menü Buttons fürs Startfenster und Startfenster.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,10 @@
-init();
 
-function init()
-{
-    background_canvas=document.getElementById('background_canvas');
-    background_ctx=background_canvas.getContext('2d');
-    main_canvas=document.getElementById('main_canvas');
-    main_ctx=main_canvas.getContext('2d');
+var canvas=null;
+var context=null;
 
+setup=function(){
+    canvas=document.getElementById("main_canvas");
+    context=main_canvas.getContext('2d');
+    
 }
 
